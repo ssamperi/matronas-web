f9adbe78ceb4ff63c5609ce2968b75e7097a209a
Sergi Samperi <sergi.sl.87@gmail.com>
2025-01-11 17:18:02 +0100

    add css and sections setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./sections/header/components/header/header.component";
+import { FooterComponent } from "./sections/footer/components/footer/footer.component";
+import { MainComponent } from "./sections/main/components/main/main.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'matronas-web';
+
 }
